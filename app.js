2274014e270080b4d32d0949e7fcaeeefc6c8c59
Marconi Calamaro <github@marconicivitavecchia.it>
2019-04-02 14:04:55 +0000

    Connect hide/show in js with php

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 var app = {
 	init: function() {
-    console.log("init");
-		$(".net-int").show();
+		console.log("init");
+		$.getJSON("location.php").done(app.showCards);
+	},
+	showCards(dataJson) {
+		console.log(dataJson);
+		if (dataJson.network==="internal") {
+			$(".net-int").show();
+		}
 	}
+
 }
 
 $(document).ready(app.init);
